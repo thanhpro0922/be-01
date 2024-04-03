6d046f6903972dec0e5ea2be5aa6d9133af3866d
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-04-03 13:22:58 +0700

    xây dựng layout chung

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
@@ -13,6 +15,8 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.locals.preFixAdmin = systemConfig.preFixAdmin;
 
 app.use(express.static("public"));
 
