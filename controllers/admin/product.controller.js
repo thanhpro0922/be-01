d1956f9d1c519c84f712c62df636d508de38f673
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-04-03 14:06:03 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../model/product-model");
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        // lấy ra những trường chưa bị xóa
+        deleted: false,
+    });
+    console.log(products);
     res.render("admin/pages/product/index", {
         pageTitle: "Trang sản phẩm",
+        products: products
     });
 };
