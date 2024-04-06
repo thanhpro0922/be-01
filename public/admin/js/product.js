54dece80fc36465cfe4934ca7a0f7a3956c55e77
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-04-07 00:15:07 +0700

    4.1 Xóa vĩnh viễn

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -20,3 +20,23 @@ if (buttonChangeStatus.length > 0) {
         });
     });
 }
+
+// Delete Item
+const buttonDelete = document.querySelectorAll("[button-delete]");
+if (buttonDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+    buttonDelete.forEach((button) => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Are you sure?"); //để user xác nhận có muốn xóa ko, khi user ấn ok sẽ trả ra true, ngược lại false
+
+            if (isConfirm) {
+                const id = button.getAttribute("data-id");
+
+                const action = `${path}/${id}?_method=DELETE`;
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            }
+        });
+    });
+}
