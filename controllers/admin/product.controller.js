bfebd70851cc1eb2837cb817a3f5850ec289151c
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-04-07 21:32:53 +0700

    3.2 Hiển thị thông báo sau khi xóa

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -86,6 +86,7 @@ module.exports.changeMulti = async (req, res) => {
                 { _id: { $in: ids } },
                 { deleted: "true", deletedAt: new Date() }
             );
+            req.flash("success", `Đã xóa thành công ${ids.length} sản phẩm!`);
             break;
         case "change-position":
             for (const item of ids) {
@@ -106,5 +107,6 @@ module.exports.deleteItem = async (req, res) => {
         { _id: id },
         { deleted: true, deletedAt: new Date() }
     );
+    req.flash("success", `Đã xóa thành công sản phẩm!`);
     res.redirect("back");
 };
