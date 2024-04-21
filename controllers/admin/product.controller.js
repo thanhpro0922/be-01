134a30715322827da3a025c19c29770438428584
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-04-21 12:14:04 +0700

    1. Validate dữ liệu

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -123,7 +123,6 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products/create
 module.exports.createPost = async (req, res) => {
-    console.log(req.file)
     req.body.price = parseInt(req.body.price);
     req.body.discountPercentage = parseInt(req.body.discountPercentage);
     req.body.stock = parseInt(req.body.stock);
@@ -135,12 +134,14 @@ module.exports.createPost = async (req, res) => {
         req.body.position = parseInt(req.body.position);
     }
 
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
+    if (req.file) {
+        req.body.thumbnail = `/uploads/${req.file.filename}`;
+    }
+
     // đoạn này để lưu dữ liệu vào database
     //đoạn này chỉ lưu ở model chứ chưa lưu vào database
     const product = new Product(req.body); // new Product nhận tham số là object, nhưng req.body nó cx trả ra object nên điền vậy
-    // đoạn này mới lưu
-    await product.save();
+    await product.save(); // đoạn này mới lưu
 
     res.redirect(`${systemConfig.preFixAdmin}/products`);
 };
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,12 +5,18 @@ const storageMulter = require("../../helpers/storageMulter");
 const upload = multer({ storage: storageMulter() });
 
 const controller = require("../../controllers/admin/product.controller");
+const validate = require("../../validate/admin/product.validate");
 
 router.get("/", controller.index);
 router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", upload.single("thumbnail"), controller.createPost); // nếu up 1 ảnh thì dùng single, nếu nhiều dùng array
+router.post(
+    "/create",
+    upload.single("thumbnail"),
+    validate.createPost,
+    controller.createPost
+); // nếu up 1 ảnh thì dùng single, nếu nhiều dùng array
 
 module.exports = router;
