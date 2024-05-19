2081821df2bef3cb265709d973ac532d67b57a94
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-05-19 20:30:27 +0700

    Tạo collection roles

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -126,29 +126,30 @@ module.exports.changeMulti = async (req, res) => {
     res.redirect("back");
 };
 
-// [GET] /admin/products/edit/:id
+// [GET] /admin/products-category/edit/:id
 module.exports.edit = async (req, res) => {
-    try {
-        const find = {
-            deleted: false,
-            _id: req.params.id,
-        };
-        const productCategory = await ProductCategory.findOne(find);
-        res.render("admin/pages/product-category/edit", {
-            pageTitle: "Chỉnh sửa sản phẩm",
-            productCategory: productCategory,
-        });
-    } catch (error) {
-        res.redirect(`${systemConfig.preFixAdmin}/products-category`);
-    }
+    const id = req.params.id;
+
+    const data = await ProductCategory.findOne({
+        _id: id,
+        deleted: false,
+    });
+
+    const records = await ProductCategory.find({
+        deleted: false, 
+    });
+    const newRecords = createTreeHelper.tree(records);
+
+    res.render("admin/pages/product-category/edit", {
+        pageTitle: "Chỉnh sửa danh mục sản phẩm",
+        data: data,
+        records: newRecords
+    });
 };
 
-// [PATCH] /admin/products/edit/:id
+// [PATCH] /admin/products-category/edit/:id
 module.exports.editPatch = async (req, res) => {
     const id = req.params.id;
-    req.body.price = parseInt(req.body.price);
-    req.body.discountPercentage = parseInt(req.body.discountPercentage);
-    req.body.stock = parseInt(req.body.stock);
     req.body.position = parseInt(req.body.position);
 
     try {
