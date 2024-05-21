e5a445fa0c32c3a4533cfe1eba0e0254b1efbe5e
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-05-21 11:46:54 +0700

    3.4 Xây dựng phần chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -28,3 +28,37 @@ module.exports.createPost = async (req, res) => {
     await record.save();
     res.redirect(`${systemConfig.preFixAdmin}/roles`);
 };
+
+// [GET] /admin/roles/edit
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        let find = {
+            _id: id,
+            deleted: false,
+        };
+
+        const data = await Role.findOne(find);
+
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Chỉnh sửa nhóm quyền",
+            data: data,
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.preFixAdmin}/roles`);
+    }
+};
+
+// [PATCH] /admin/roles/edit
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Role.updateOne({ _id: id }, req.body);
+        req.flash("success", "Cập nhật nhóm quyền thành công");
+    } catch (error) {
+        res.flash("error", "Cập nhật nhóm quyền thất bại");
+    }
+    res.redirect("back");
+};
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -6,4 +6,6 @@ const controller = require("../../controllers/admin/role.controller");
 router.get("/", controller.index);
 router.get("/create", controller.create);
 router.post("/create", controller.createPost);
+router.get("/edit/:id", controller.edit);
+router.patch("/edit/:id", controller.editPatch);
 module.exports = router;
