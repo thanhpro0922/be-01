636d2caffe0dddb09079641eefb585b2fee0d1dc
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-16 10:37:26 +0700

    1.5. Làm trang sửa tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -54,3 +54,52 @@ module.exports.createPost = async (req, res) => {
         res.redirect(`${systemConfig.preFixAdmin}/accounts`);
     }
 };
+
+// [GET] /admin/accounts/edit
+module.exports.edit = async (req, res) => {
+    let find = {
+        _id: req.params.id,
+        deleted: false,
+    };
+
+    try {
+        const data = await Account.findOne(find);
+        const roles = await Role.find({
+            deleted: false,
+        });
+
+        res.render("admin/pages/accounts/edit", {
+            pageTitle: "Chỉnh sửa tài khoản",
+            data: data,
+            roles: roles,
+        });
+    } catch (error) {
+        res.redirect(`/${systemConfig.preFixAdmin}/accounts`);
+    }
+};
+
+// [PATCH] /admin/accounts/edit
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    const emailExist = await Account.findOne({
+        _id: { $ne: id }, // $ne là viết tắt của not equal tức là không bằng, nghĩa là loại trừ trường hợp id bằng id này
+        email: req.body.email,
+        deleted: false,
+    });
+
+    if (emailExist) {
+        req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+        res.redirect("back");
+    } else {
+        if (req.body.password) {
+            req.body.password = md5(req.body.password);
+        } else {
+            delete req.body.password;
+        }
+        await Account.updateOne({ _id: id }, req.body);
+        req.flash("success", "Cập nhật thành công!");
+
+        res.redirect("back");
+    }
+};
