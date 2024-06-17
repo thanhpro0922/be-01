6182abdaa6df2263c5577ad29ebcad215ab739ff
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-17 09:09:49 +0700

    1. Thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,13 +1,21 @@
-const systemConfig = require("../../config/system");
 const Account = require("../../model/account.model");
+const Role = require("../../model/role.model");
+const systemConfig = require("../../config/system");
 module.exports.requireAuth = async (req, res, next) => {
     if (!req.cookies.token) {
         res.redirect(`${systemConfig.preFixAdmin}/auth/login`);
     } else {
-        const user = await Account.findOne({ token: req.cookies.token });
+        const user = await Account.findOne({ token: req.cookies.token }).select(
+            "-password"
+        );
         if (!user) {
             res.redirect(`${systemConfig.preFixAdmin}/auth/login`);
         } else {
+            const role = await Role.findOne({
+                _id: user.role_id,
+            }).select("title permissions");
+            res.locals.user = user; // locals là trả về toàn cục - nghĩa là ở đâu cũng xài được cái user
+            res.locals.role = role;
             next();
         }
     }
