7966292751de161faf6940310c4812caa38297de
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-19 23:23:10 +0700

    1. Làm trang Thông tin cá nhân bên admin

---
+++ controllers/admin/my-account.controller.js
@@ -0,0 +1,41 @@
+const md5 = require("md5");
+const Account = require("../../model/account.model");
+//TODO [GET] /admin/my-account/
+module.exports.index = async (req, res) => {
+    res.render("admin/pages/my-account/index", {
+        pageTitle: "Thông tin cá nhân",
+    });
+};
+
+//TODO [GET] /admin/my-account/edit
+module.exports.edit = async (req, res) => {
+    res.render("admin/pages/my-account/edit", {
+        pageTitle: "Chỉnh sửa thông tin cá nhân",
+    });
+};
+
+//TODO [PATCH] /admin/my-account/edit
+module.exports.editPatch = async (req, res) => {
+    const id = res.locals.user.id;
+
+    const emailExist = await Account.findOne({
+        _id: { $ne: id }, //! $ne là viết tắt của not equal tức là không bằng, nghĩa là loại trừ trường hợp id bằng id này
+        email: req.body.email,
+        deleted: false,
+    });
+
+    if (emailExist) {
+        req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+        res.redirect("back");
+    } else {
+        if (req.body.password) {
+            req.body.password = md5(req.body.password);
+        } else {
+            delete req.body.password;
+        }
+        await Account.updateOne({ _id: id }, req.body);
+        req.flash("success", "Cập nhật thành công!");
+
+        res.redirect("back");
+    }
+};
