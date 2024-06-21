2131f90c40dab816c2f743f6028254d46576037d
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-22 05:15:32 +0700

    4.2 Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,17 +3,31 @@ const productsHelper = require("../../helpers/product");
 
 //  [GET] /
 module.exports.index = async (req, res) => {
-    // Lấy ra sản phẩm nỏi bật
+    // todo Lấy ra sản phẩm nỏi bật
     const productsFeatured = await Product.find({
         featured: "1",
         deleted: false,
         status: "active",
-    }).limit(3);
-    const newProduct = productsHelper.priceNewProducts(productsFeatured);
+    }).limit(6);
+    const newProductFeatured =
+        productsHelper.priceNewProducts(productsFeatured);
+    // todo End Lấy ra sản phẩm nỏi bật
 
-    // End Lấy ra sản phẩm nỏi bật
+    // todo Hiển thị danh sách sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active",
+    })
+        .sort({ position: "desc" })
+        .limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
+
+    // todo End Hiển thị danh sách sản phẩm mới nhất
+
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProduct,
+        productsFeatured: newProductFeatured,
+        productsNew: newProductsNew,
     });
 };
