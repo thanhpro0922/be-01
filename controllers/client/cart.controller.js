e789a08a331c3c7e6662318fab688d9c152c94ce
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-25 00:41:01 +0700

    2.3 Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,40 @@
 const Cart = require("../../model/cart.model");
+const Product = require("../../model/product-model");
+
+const productsHelper = require("../../helpers/product");
+
+//@ [GET] /card/
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const cart = await Cart.findOne({
+        _id: cartId,
+    });
+
+    if (cart.products.length > 0) {
+        for (const item of cart.products) {
+            const productId = item.product_id;
+
+            const productInfo = await Product.findOne({
+                _id: productId,
+            });
+
+            productInfo.priceNew = productsHelper.priceNewProduct(productInfo);
+
+            item.productInfo = productInfo;
+            item.totalPrice = item.quantity * productInfo.priceNew; //@ Đây là tổng tiền của mỗi item
+        }
+    }
+
+    cart.totalPrice = cart.products.reduce(
+        (sum, item) => sum + item.totalPrice,
+        0
+    ); //@ Đây là tổng tiền của cả giỏ hàng
+
+    res.render("client/pages/cart/index", {
+        pageTitle: "Giỏ hàng",
+        cartDetail: cart,
+    });
+};
 
 //@ [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
