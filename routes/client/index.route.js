8bf89538e05a4e5e8222dcfacbfa4f05ab63a789
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-25 02:27:22 +0700

    1.2 Làm giao diện

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const homeRoutes = require("./home.route");
 const productRoutes = require("./products.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
@@ -14,4 +15,5 @@ module.exports = (app) => {
     app.use("/products", productRoutes);
     app.use("/search", searchRoutes);
     app.use("/cart", cartRoutes);
+    app.use("/checkout", checkoutRoutes);
 };
