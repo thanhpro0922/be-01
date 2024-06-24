1e4a4b89332bb99f6066b9fe9f4555456ae2924f
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-25 03:00:35 +0700

    1.3 Lấy data được gửi lên lưu vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,8 +1,10 @@
 const Cart = require("../../model/cart.model");
 const Product = require("../../model/product-model");
+const Order = require("../../model/order.model");
 
 const productsHelper = require("../../helpers/product");
 
+//@ [GET] /checkout/
 module.exports.index = async (req, res) => {
     const cartId = req.cookies.cartId;
     const cart = await Cart.findOne({
@@ -33,4 +35,49 @@ module.exports.index = async (req, res) => {
         pageTitle: "Đặt hàng",
         cartDetail: cart,
     });
+};
+
+//@ [POST] /checkout/order
+module.exports.order = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const cart = await Cart.findOne({
+        _id: cartId,
+    });
+
+    let products = [];
+
+    for (const product of cart.products) {
+        const objectProduct = {
+            product_id: product.product_id,
+            price: 0,
+            discountPercentage: 0,
+            quantity: product.quantity,
+        };
+        const productInfo = await Product.findOne({
+            _id: product.product_id,
+        });
+        objectProduct.price = productInfo.price;
+        objectProduct.discountPercentage = productInfo.discountPercentage;
+        products.push(objectProduct);
+    }
+    const objectOrder = {
+        cart_id: cartId,
+        userInfo: userInfo,
+        products: products,
+    };
+
+    const order = new Order(objectOrder);
+    await order.save();
+
+    await Cart.updateOne(
+        {
+            _id: cartId,
+        },
+        {
+            products: [],
+        }
+    );
+    res.redirect(`/checkout/success/${order.id}`);
 };
