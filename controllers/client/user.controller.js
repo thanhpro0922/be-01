df1bd0b14f95a16ec3eaf430151dd69f313701fb
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-26 06:57:10 +0700

    2.4 Đăng xuất

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -61,5 +61,11 @@ module.exports.loginPost = async (req, res) => {
     }
 
     res.cookie("tokenUser", user.tokenUser);
-    res.send("OK");
+    res.redirect("/");
+};
+
+//@ [GET] /user/logout
+module.exports.logout = async (req, res) => {
+    res.clearCookie("tokenUser");
+    res.redirect("/");
 };
--- middlewares/client/user.middleware.js
+++ middlewares/client/user.middleware.js
@@ -6,7 +6,6 @@ module.exports.infoUser = async (req, res, next) => {
             tokenUser: req.cookies.tokenUser,
             deleted: false,
         }).select("-password");
-        console.log(user);
         if (user) {
             res.locals.user = user;
         }
