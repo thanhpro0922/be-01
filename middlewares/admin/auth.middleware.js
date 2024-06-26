4a8cdb6cb9e347edd71ff90eb837a629ef2846ff
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-06-27 03:54:37 +0700

    Nãy sửa nhầm cái middleware bên admin giờ fix lại

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -3,12 +3,12 @@ const Role = require("../../model/role.model");
 const systemConfig = require("../../config/system");
 module.exports.requireAuth = async (req, res, next) => {
     if (!req.cookies.token) {
-        res.redirect(`/${systemConfig.preFixAdmin}/auth/login`);
+        res.redirect(`${systemConfig.preFixAdmin}/auth/login`);
         return;
     }
     const user = await Account.findOne({ token: req.cookies.token });
     if (!user) {
-        res.redirect(`/${systemConfig.preFixAdmin}/auth/login`);
+        res.redirect(`${systemConfig.preFixAdmin}/auth/login`);
         return;
     }
     const role = await Role.findOne({
